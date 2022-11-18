a8484d1da9e744150649ae9e878a07abbd57ae7b
Petr <sppetrak@mail.ru>
2022-11-18 20:27:46 +0300

    Решение второй задачи

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число от 0 до 10000 ");
+string numbers = Console.ReadLine()!;
+
+if(numbers.Length <= 2)
+{
+    Console.WriteLine("Третьей цифры нет в введенном числе ");
+}
+else 
+{
+    Console.WriteLine($"Третья цифра введенного числа = {numbers[2]}");
+}
+
