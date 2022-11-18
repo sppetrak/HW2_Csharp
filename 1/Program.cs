b399b1bd0b6964787cbbd7142c466574a60e0e6d
Petr <sppetrak@mail.ru>
2022-11-18 20:12:23 +0300

    Решение первой задачи

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 3х значное число ");
+string numbers = Console.ReadLine()!;
+
+if(numbers.Length != 3)
+{
+    Console.WriteLine("Введено неверное число ");
+}
+else 
+{
+    Console.WriteLine($"Вторая цифра введенного числа = {numbers[1]}");
+}
+
