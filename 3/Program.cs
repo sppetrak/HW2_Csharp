dd98b3da26ce430a3163f2a3d5495c793e866292
Petr <sppetrak@mail.ru>
2022-11-18 20:33:27 +0300

    Решение Третьей  задачи

---
+++ 3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите номер дня недели ");
+int day = int.Parse(Console.ReadLine()!);
+if(day== 1)
+{
+    Console.Write("Нет( ");
+}
+if(day== 2)
+{
+    Console.Write("Нет( ");
+}if(day== 3)
+{
+    Console.Write("Нет( ");
+}if(day== 4)
+{
+    Console.Write("Нет( ");
+}if(day== 5)
+{
+    Console.Write("Нет( ");
+}if(day== 6)
+{
+    Console.Write("ДА )))");
+}if(day== 7)
+{
+    Console.Write("ДА )))");
+}
